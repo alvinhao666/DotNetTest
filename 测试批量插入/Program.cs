72200e71f2c71a842474f2249a075f0b43f0c599
rongguohao <rongguohao@vip56.cn>
2019-06-14 13:24:46 +0800

    fix #etc

--- 测试批量插入/Program.cs
+++ 测试批量插入/Program.cs
@@ -6,6 +6,7 @@ using Sino.Dapper;
 using Microsoft.Extensions.DependencyInjection;
 using Sino.Hf.EtcService;
 using 爬取电影天堂;
+using System.Linq;
 
 namespace 测试批量插入
 {
@@ -16,7 +17,7 @@ namespace 测试批量插入
             IServiceCollection services = new ServiceCollection();
 
 
-            services.AddDapper("Data Source=47.96.143.165;port=3306;user id=root;password=5802486;Initial Catalog=tmsystem;convertzerodatetime=True;AutoEnlist=false;Charset=utf8;", "Data Source=47.96.143.165;port=3306;user id=root;password=5802486;Initial Catalog=tmsystem;convertzerodatetime=True;AutoEnlist=false;Charset=utf8;");
+            services.AddDapper("Data Source=rm-bp1sxihspw88jl9fp2o.mysql.rds.aliyuncs.com;port=3306;user id=sino;password=sino5802486A;Initial Catalog=tmsystem;convertzerodatetime=True;AutoEnlist=false;Charset=utf8;", "Data Source=rm-bp1sxihspw88jl9fp2o.mysql.rds.aliyuncs.com;port=3306;user id=sino;password=sino5802486A;Initial Catalog=tmsystem;convertzerodatetime=True;AutoEnlist=false;Charset=utf8;");
 
             //注入
             //services.AddTransient<IIdentifyProvider, IdentifyProvider>();
@@ -101,7 +102,19 @@ namespace 测试批量插入
 
             //await rep.InsertList(list);
 
-            var item = await rep.GetCarrierOrderDetail(Guid.NewGuid());
+            var item = await rep.GetCarrierOrderDetail(Guid.Parse("7dd9599f-b309-43e4-8952-1c825c94426c"));
+
+            var order = item.OrderList.OrderBy(a => a.OrderId).FirstOrDefault();
+            var num = item.CarrierOrderCode;//运单编号
+                var plateNum = item.CarCode;
+                var plateColor = (int)CarPlateColor.Yellow;
+                var startTime = order.RealDeliveryTime.Value.ToString();//运单开始时间
+                var sourceAddr = order.OriginAddress;//运单开始地址
+                var destAddr = order.DestinationAddress;//运单目的地址
+                var predictEndTime = order.ArrivalTime.ToString();//运单预计完成时间
+                var fee = Convert.ToInt64(item.Contract.TotalPrice * 100);//大于0的整数，单位：分
+                var titleType = 1;
+
 
             Console.WriteLine("成功");
             Console.ReadKey();
