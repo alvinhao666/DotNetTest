81d495d093b9ca5d89ea53ecff62e937add8f27b
rongguohao <rongguohao@vip56.cn>
2019-06-20 13:52:50 +0800

    1

---
+++ Readonly/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example02Lib
+{
+    public class Class1
+    {
+        public const string strConst = "Const";
+        public static readonly string strStaticReadonly = "StaticReadonly";
+        //public const string strConst = "Const Changed";
+        //public static readonly string strStaticReadonly = "StaticReadonly Changed";
+    }
+}
--- Readonly/Program.cs
+++ Readonly/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Example02Lib;
+using System;
 
 namespace Readonly
 {
@@ -6,10 +7,22 @@ namespace Readonly
     {
         static void Main(string[] args)
         {
-            TestReadonly item = new TestReadonly();
-            int x=item.GetX();
+            //TestReadonly item = new TestReadonly();
+            //int x=item.GetX();
 
+ 
+            //修改Example02中Class1的strConst初始值后，只编译Example02Lib项目            
+            //然后到资源管理器里把新编译的Example02Lib.dll拷贝Example02.exe所在的目录，执行Example02.exe            
+            //切不可在IDE里直接调试运行因为这会重新编译整个解决方案！！             
+            //可以看到strConst的输出没有改变，而strStaticReadonly的输出已经改变            
+            //表明Const变量是在编译期初始化并嵌入到客户端程序，而StaticReadonly是在运行时初始化的            
+            Console.WriteLine("strConst : {0}", Class1.strConst);
+            Console.WriteLine("strStaticReadonly : {0}", Class1.strStaticReadonly);
             Console.ReadKey();
+            Console.WriteLine("strConst : {0}", Class1.strConst);
+            Console.WriteLine("strStaticReadonly : {0}", Class1.strStaticReadonly);
+            Console.ReadKey();
+
         }
     }
 
