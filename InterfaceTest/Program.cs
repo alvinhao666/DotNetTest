739091ca0a1cd739abee47c8181e0f4e8fe9d608
rongguohao <rongguohao@vip56.cn>
2019-06-20 10:01:13 +0800

    1

--- InterfaceTest/Program.cs
+++ InterfaceTest/Program.cs
@@ -11,7 +11,11 @@ namespace InterfaceTest
 
             IPerson p2 = new Person2();
 
-            Console.WriteLine(p1.GetType().Name);
+            var t = p1.GetType();
+            Console.WriteLine(t.GetFields()[0]); //Fields字段
+            Console.WriteLine(t.GetProperties()[0]);
+            Console.WriteLine(t.GetNestedTypes()[0]);
+            Console.WriteLine(t.IsGenericType);
             Console.WriteLine(p2.GetType().FullName);
 
             Console.ReadKey();
@@ -26,7 +30,14 @@ namespace InterfaceTest
 
     public class Person1 : IPerson
     {
+        public int Name;//Fields字段
         public int PersonType { get; set; }
+        
+
+        public class Classes
+        {
+            public string Name { get; set; }
+        }
     }
 
     public class Person2 : IPerson
