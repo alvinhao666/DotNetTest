689437845954e238a528333b9a7eb58c88c7c0c3
rongguohao <rongguohao@vip56.cn>
2019-06-27 14:01:23 +0800

    1

---
+++ SelectAndSelectMany/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace SelectAndSelectMany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = { "Albert was here", "Burke slept late", "Connor is happy" };
+            var tokens = text.Select(s => s.Split(" "));
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (string[] line in tokens)
+            {
+                foreach (string token in line)
+                {
+                    Console.WriteLine("{0}", token);
+                }
+            }
+
+
+            string[] text2 = { "Albert was here", "Burke slept late", "Connor is happy" };
+            Console.ForegroundColor = ConsoleColor.Red;
+            var tokens2 = text2.SelectMany(s => s.Split(' '));
+            foreach (string token in tokens2)
+            {
+                Console.WriteLine("{0}", token);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Zhongqian/Program.cs
+++ Zhongqian/Program.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zhongqian
+namespace TaskTest
 {
     class Program
     {
@@ -23,31 +23,8 @@ namespace Zhongqian
 
                 tasks.Add(Test(m));
             }
-            //// Wait for all tasks to complete.
             Task.WaitAll(tasks.ToArray());
-            //when Queue<Book> is empty,reload data...
-
-            //when Cur>=Max,break the while loop
-            Console.WriteLine(HMACSHA256("jmsexxw"));
-
-
-            //string[] text = { "Albert was here", "Burke slept late", "Connor is happy" };
-            //var tokens = text.Select(s => s.Split(" "));
-            //foreach (string[] line in tokens)
-            //    foreach (string token in line)
-            //        Console.Write("{0}.", token);
-
-
-            string no = "061407";
-            Console.WriteLine(no.Substring(0, 2));
-            Console.WriteLine(no.Substring(2, 2));
-            Console.WriteLine(no.Substring(4, 2));
-
-            //string[] text = { "Albert was here", "Burke slept late", "Connor is happy" };
-            //var tokens = text.SelectMany(s => s.Split(' '));
-            //foreach (string token in tokens)
-            //    Console.Write("{0}.", token);
-
+       
             Console.ReadKey();
         }
 
@@ -57,23 +34,6 @@ namespace Zhongqian
             Console.WriteLine(num.ToString());
 
             await Task.Factory.StartNew(() => { Console.WriteLine($"{num * 20}"); });
-        }
-
-        public static string HMACSHA256(string srcString, string key = "Es6E3Fg/58kEOPKyi0X3+w==")
-        {
-            byte[] secrectKey = Encoding.UTF8.GetBytes(key);
-            using (HMACSHA256 hmac = new HMACSHA256(secrectKey))
-            {
-                hmac.Initialize();
-
-                byte[] bytes_hmac_in = Encoding.UTF8.GetBytes(srcString);
-                byte[] bytes_hamc_out = hmac.ComputeHash(bytes_hmac_in);
-
-                string str_hamc_out = BitConverter.ToString(bytes_hamc_out);
-                str_hamc_out = str_hamc_out.Replace("-", "");
-
-                return str_hamc_out;
-            }
         }
 
     }
