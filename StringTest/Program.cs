f069d6166299f6e8e39667c4b6294d67289a5542
rongguohao <rongguohao@vip56.cn>
2019-07-12 15:58:09 +0800

    1

--- StringTest/Program.cs
+++ StringTest/Program.cs
@@ -13,11 +13,31 @@ namespace StringTest
             Console.WriteLine("12312&nbsp;vfdf&nbsp;sdf&nbsp;sdfdcc".Split(new String[1] { "&nbsp;" }, 8, StringSplitOptions.None)[0]);
 
             Console.WriteLine("12312&nbsp;vfdf&nbsp;sdf&nbsp;sdfdcc".Split("1").FirstOrDefault()); //以开头字母分割 会有空格
-
+            #endregion
 
             #region 过滤html标签
             string strhtml = NoHTML("</font>,&nbsp;<font face=\"Arial\"><span style=\"font - size: 14px; line - height: 21.59375px; \">,  满清康熙鼎盛之际，自十三岁入宫当宫女的兆佳沉香（");
             #endregion
+
+            #region 地址拆分
+
+            string address = "江苏省镇江市京口区梦溪嘉苑";
+
+            string province = "";
+
+            string city = "";
+
+            string district = "";
+
+            string regex = "(?<province>[^省]+省|[^自治区]+自治区|.+市)(?<city>[^自治州]+自治州|.+区划|[^市]+市|[^盟]+盟|[^地区]+地区)?(?<county>[^市]+市|[^县]+县|[^旗]+旗|.+区)?(?<town>[^区]+区|.+镇)?(?<village>.*)";
+
+
+            foreach (Match match in Regex.Matches(address, regex))
+            {
+                province = match.Groups[1].Value;
+                city = match.Groups[2].Value;
+                district = match.Groups[3].Value;
+            }
             #endregion
 
             Console.ReadKey();
