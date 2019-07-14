3762cd1ec056aa287cb7be5ea4acc99754c46fd2
rongguohao <843468011@qq.com>
2019-07-14 18:14:25 +0800

    `

---
+++ Configuaration读取/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading;
+
+namespace Configuration读取
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory)
+                                                                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true) //热加载，\bin\Debug\netcoreapp2.2\appsettings.json
+                                                                    .Build();
+
+
+            while(true)
+            {
+                var info = configuration["username"];
+
+                var ip = configuration["mysql:ip"];
+
+                Console.WriteLine(info);
+
+                Console.WriteLine(ip);
+
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
