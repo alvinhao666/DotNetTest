46c93b30b1349ec5cbab3cf26d0d5cf5b24967fa
rongguohao <rongguohao@vip56.cn>
2019-08-19 10:42:09 +0800

    1

---
+++ Dynamic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Sample
+    {
+        public string Name { get; set; }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
