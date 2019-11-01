2ae1e1e4e1268644f7a34fd77391556b83bc9c11
rongguohao <rongguohao@vip56.cn>
2019-11-01 10:17:57 +0800

    1

---
+++ GetValueOrDefault/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GetValueOrDefault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = 1;
+
+            int? b = null;
+
+            Console.WriteLine(a.GetValueOrDefault());
+            Console.WriteLine(b.GetValueOrDefault());
+            Console.ReadKey();
+        }
+    }
+}
