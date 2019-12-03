dcafd3f2eec8b56c8557802effe3f4e04ce850a5
rongguohao <rongguohao@vip56.cn>
2019-12-03 09:15:14 +0800

    1

---
+++ BlockingCollection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlockingCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockingCollection<string> blockingCollection = new BlockingCollection<string>();
+            ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+            var t = new Task[50];
+            for (int i = 0; i <= 49; i++)
+            {
+                t[i] = Task.Factory.StartNew((obj) =>
+                {
+                    Thread.Sleep(2500);
+                    blockingCollection.Add(obj.ToString());
+                    queue.Enqueue(obj.ToString());
+                    Console.WriteLine("Task中的数据: {0}", obj.ToString());
+                }, i + 1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- 简易消息队列/DemoQueueBlock.cs
+++ 简易消息队列/DemoQueueBlock.cs
@@ -7,15 +7,13 @@ namespace 简易消息队列
 {
     public class DemoQueueBlock<T> where T:class
     {
-        private static BlockingCollection<T> Colls; //一个支持界限和阻塞的容器（线程安全集合）
+        //BlockingCollection集合是一个拥有阻塞功能的集合，它就是完成了经典生产者消费者的算法功能。一般情况下，我们可以基于 生产者 - 消费者模式来实现并发。BlockingCollection<T> 类是最好的解决方案
+        private static BlockingCollection<T> Colls = new BlockingCollection<T>(); //一个支持界限和阻塞的容器（线程安全集合）
 
-        public DemoQueueBlock()
-        {
 
-        }
         public static bool IsComleted()
         {
-            if (Colls != null && Colls.IsCompleted)
+            if (Colls.IsCompleted)
             {
                 return true;
             }
@@ -23,7 +21,7 @@ namespace 简易消息队列
         }
         public static bool HasEle()
         {
-            if (Colls != null && Colls.Count > 0)
+            if (Colls.Count > 0)
             {
                 return true;
             }
@@ -32,19 +30,11 @@ namespace 简易消息队列
 
         public static bool Add(T msg)
         {
-            if (Colls == null)
-            {
-                Colls = new BlockingCollection<T>();
-            }
             Colls.Add(msg);
             return true;
         }
         public static T Take()
         {
-            if (Colls == null)
-            {
-                Colls = new BlockingCollection<T>();
-            }
             return Colls.Take();
         }
     }
