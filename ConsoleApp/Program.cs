038ca57bb9925306731a99dbb9da2c9b1f714384
rongguohao <rongguohao@vip56.cn>
2020-05-28 16:19:10 +0800

    1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -204,7 +204,13 @@ namespace ConsoleApp3
 
             Console.WriteLine(float.Parse("31.237212"));
 
+            int? value = 0;
+            value++;
+            Console.WriteLine(value);
 
+            string code = "苏L0L937";
+
+            var flag=Regex.IsMatch(code, @"^(([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z](([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]))$");
 
 
             Console.ReadKey();
