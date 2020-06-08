ca7f97ae3a00c05f37ec32f118dfc7824159317b
rongguohao <rongguohao@vip56.cn>
2020-06-08 13:57:23 +0800

    1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -213,6 +213,11 @@ namespace ConsoleApp3
             var flag=Regex.IsMatch(code, @"^(([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z](([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]))$");
 
 
+            Console.WriteLine(Convert.ToInt64(Math.Pow(2, 0)));
+
+
+            Console.WriteLine("".Substring(0, 2));
+
             Console.ReadKey();
         }
 
