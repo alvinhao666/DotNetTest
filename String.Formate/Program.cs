9b3f178977636f3ea4969edd266f3d619afaa61a
rongguohao <rongguohao@vip56.cn>
2020-08-28 14:39:59 +0800

    1

---
+++ String.Formate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace String.Formate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(string.Format("{0:D4}", 1)); //0001
+            Console.WriteLine(string.Format("{0:D4}", "1")); //1
+
+            Console.WriteLine(1223.234323423.ToString("#0.####")); //1223.2343
+            Console.WriteLine(1223.2000.ToString("#0.####")); //1223.2
+            Console.WriteLine(1223.0000.ToString("#0.####")); //1223
+            Console.ReadKey();
+        }
+    }
+}
