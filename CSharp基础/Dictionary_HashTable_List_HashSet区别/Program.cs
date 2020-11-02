a62e4539c0b9548d7074c0f21f4ea9da5b2d0cc7
rongguohao <rongguohao@vip56.cn>
2020-11-02 16:43:19 +0800

    1

--- CSharp基础/Dictionary_HashTable_List_HashSet区别/Program.cs
+++ CSharp基础/Dictionary_HashTable_List_HashSet区别/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Dictionary_HashTable_List_HashSet区别
 {
@@ -6,7 +9,28 @@ namespace Dictionary_HashTable_List_HashSet区别
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<Student> hashSet = new HashSet<Student>();
+
+            var s1 = new Student { Name = "张三", Age = 20 };
+
+            hashSet.Add(s1);
+            hashSet.Add(s1);
+
+            var s2 = new Student { Name = "张三", Age = 20 };
+            hashSet.Add(s2);
+
+            Console.WriteLine(JsonConvert.SerializeObject(hashSet)); //输出两个张三
+
+
+            Console.ReadKey();
         }
+    }
+
+
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 }
