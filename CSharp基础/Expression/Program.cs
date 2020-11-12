634b3c3638228879f3d0ee0aeb296beceab3a5a1
rongguohao <rongguohao@vip56.cn>
2020-11-12 14:34:28 +0800

    1

--- CSharp基础/Expression/Program.cs
+++ CSharp基础/Expression/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Dynamic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace Test
@@ -21,7 +24,25 @@ namespace Test
             Expression<Action<string>> _consStringExp = Expression.Lambda<Action<string>>(_methodCallexpP, _parameExp);
             _consStringExp.Compile()("Hello!!");
 
+            User user = new User { Name = "123" };
+
+            UpdateAsync<User>(user => new {  });
+
             Console.ReadLine();
         }
+
+
+        static void UpdateAsync<T>(Expression<Func<T, object>> columns)
+        {
+
+            var properties = columns.Body.Type.GetProperties();
+            Console.WriteLine(properties.Length);
+        }
+    }
+
+
+    public class User
+    {
+        public string Name { get; set; }
     }
 }
