a19a83e082ca70877317892d7ff9e8f7db8595d2
rongguohao <rongguohao@vip56.cn>
2021-03-18 16:57:56 +0800

    1

---
+++ 有序不重复GUID/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 有序不重复GUID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 1000; i++)
+            {
+                Console.WriteLine(Util.NewMongodbId());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 有序不重复GUID/Util.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 有序不重复GUID
+{
+    public static class Util
+    {
+
+        private static DateTime dt1970 = new DateTime(1970, 1, 1);
+        private static ThreadLocal<Random> rnd = new ThreadLocal<Random>(() => new Random());
+        private static readonly int __staticMachine = ((0x00ffffff & Environment.MachineName.GetHashCode()) +
+#if NETSTANDARD1_5 || NETSTANDARD1_6
+			1
+#else
+            AppDomain.CurrentDomain.Id
+#endif
+            ) & 0x00ffffff;
+        private static readonly int __staticPid = Process.GetCurrentProcess().Id;
+        private static int __staticIncrement = rnd.Value.Next();
+        /// <summary>
+        /// 生成类似Mongodb的ObjectId有序、不重复Guid
+        /// </summary>
+        /// <returns></returns>
+        public static Guid NewMongodbId()
+        {
+            var now = DateTime.Now;
+            var uninxtime = (int)now.Subtract(dt1970).TotalSeconds;
+            int increment = Interlocked.Increment(ref __staticIncrement) & 0x00ffffff;
+            var rand = rnd.Value.Next(0, int.MaxValue);
+            var guid = $"{uninxtime.ToString("x8").PadLeft(8, '0')}{__staticMachine.ToString("x8").PadLeft(8, '0').Substring(2, 6)}{__staticPid.ToString("x8").PadLeft(8, '0').Substring(6, 2)}{increment.ToString("x8").PadLeft(8, '0')}{rand.ToString("x8").PadLeft(8, '0')}";
+            return Guid.Parse(guid);
+        }
+    }
+}
