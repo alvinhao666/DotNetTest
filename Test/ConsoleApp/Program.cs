e66f93fe5cf1e95d572fcb17f6b33bcd3b1fef0c
rongguohao <rongguohao@vip56.cn>
2021-04-21 10:10:43 +0800

    1

---
+++ Test/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace ConsoleApp3
+{
+    ////////////////////////////////////////////////////////////////////
+    //                          _ooOoo_                               //
+    //                         o8888888o                              //
+    //                         88" . "88                              //
+    //                         (| ^_^ |)                              //
+    //                         O\  =  /O                              //
+    //                      ____/`---'\____                           //
+    //                    .'  \\|     |//  `.                         //
+    //                   /  \\|||  :  |||//  \                        //
+    //                  /  _||||| -:- |||||-  \                       //
+    //                  |   | \\\  -  /// |   |                       //
+    //                  | \_|  ''\---/''  |   |                       //
+    //                  \  .-\__  `-`  ___/-. /                       //
+    //                ___`. .'  /--.--\  `. . ___                     //
+    //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+    //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+    //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+    //      ========`-.____`-.___\_____/___.-`____.-'========         //
+    //                           `=---='                              //
+    //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+    //                   佛祖保佑       永不宕机     永无BUG            //
+    ////////////////////////////////////////////////////////////////////
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
