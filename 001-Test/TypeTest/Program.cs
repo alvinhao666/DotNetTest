5798f2934654d8dfd1120289ab36cbcfe1cd2de4
rongguohao <843468011@qq.com>
2021-11-10 10:30:25 +0800

    init

---
+++ 001-Test/TypeTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TypeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(typeof(int)); //System.Int32
+            Console.WriteLine(typeof(int?)); //System.Nullable`1[System.Int32]
+
+            Console.WriteLine(typeof(int).Namespace); //System
+            Console.WriteLine(typeof(int?).Namespace); //System
+            Console.WriteLine(typeof(int).Name); //Int32
+            Console.WriteLine(typeof(int?).Name); //Nullable`1
+
+            Console.WriteLine(typeof(uint));//System.UInt32
+            Console.WriteLine(typeof(uint?));//System.Nullable`1[System.UInt32]
+
+            Console.WriteLine(typeof(uint).Name); //UInt32
+            Console.WriteLine(typeof(uint?).Name); //Nullable`1
+
+            Console.WriteLine(typeof(string));
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
