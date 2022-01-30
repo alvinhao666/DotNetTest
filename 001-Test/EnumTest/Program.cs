0e016791df07fec5c07edd554cf13da041330f6f
rongguohao <843468011@qq.com>
2022-01-30 12:57:48 +0800

    1

--- 001-Test/EnumTest/Program.cs
+++ 001-Test/EnumTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Sino.CapacityCloud.Core;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Reflection;
 
 namespace EnumTest
@@ -9,78 +11,107 @@ namespace EnumTest
     {
         static void Main(string[] args)
         {
-            var users = new List<User>() { new User() { type = UserType.typeX } };
+            //var users = new List<User>() { new User() { type = UserType.typeX } };
 
-            UserType d = (UserType)4;
+            //UserType d = (UserType)4;
 
-            Array arrays = Enum.GetValues(typeof(UserType));
-            Console.WriteLine(arrays.GetValue(0).ToString());
+            //Array arrays = Enum.GetValues(typeof(UserType));
+            //Console.WriteLine(arrays.GetValue(0).ToString());
 
-            User s = new User() { type = UserType.typeX };
-            User ss = new User() { type = UserType.typeY };
-            var sss = s.type.ToString();
-            s.type = (UserType)4;
-            Console.WriteLine(string.Format("{0} {1}", s.type, ss.type));
+            //User s = new User() { type = UserType.typeX };
+            //User ss = new User() { type = UserType.typeY };
+            //var sss = s.type.ToString();
+            //s.type = (UserType)4;
+            //Console.WriteLine(string.Format("{0} {1}", s.type, ss.type));
 
-            UserType? sa = null;
-            Console.WriteLine(1);
-            Console.WriteLine(sa.GetValueOrDefault()); //getvalueordefault
+            //UserType? sa = null;
+            //Console.WriteLine(1);
+            //Console.WriteLine(sa.GetValueOrDefault()); //getvalueordefault
 
-            //FieldInfo[] fields = typeof(VehicleClassDetail).GetFields();
-            //for (int i = 0; i < fields.Length; i++)
+            ////FieldInfo[] fields = typeof(VehicleClassDetail).GetFields();
+            ////for (int i = 0; i < fields.Length; i++)
+            ////{
+            ////    if (i > 0)
+            ////        Console.WriteLine(fields[i] + "--->" + (int)fields[i].GetValue(null));
+            ////    else
+            ////        Console.WriteLine(fields[i]);
+            ////}
+
+
+            //System.Array values = System.Enum.GetValues(typeof(VehicleClassDetail));
+            //foreach (var value in values)
             //{
-            //    if (i > 0)
-            //        Console.WriteLine(fields[i] + "--->" + (int)fields[i].GetValue(null));
-            //    else
-            //        Console.WriteLine(fields[i]);
+            //    Console.WriteLine(value + "--" + (int)value);//获取名称和值
             //}
 
+            ////Type? ssss = null;
 
-            System.Array values = System.Enum.GetValues(typeof(VehicleClassDetail));
-            foreach (var value in values)
+            ////int i = (int)ssss;
+
+            //CarType result;
+            //Console.WriteLine("CarType100");
+            //Console.WriteLine(Enum.Parse<CarType>("100")); // 100
+            //Console.WriteLine(Enum.TryParse("C1", out result)); //true
+            //Console.WriteLine(Enum.TryParse("C5", out result)); //false
+            //Console.WriteLine(Enum.TryParse("0", out result)); //true
+            //Console.WriteLine(Enum.TryParse("10", out result)); //true
+
+            //Console.WriteLine("-10" + Enum.TryParse("-10", out result)); //true
+
+            //Console.WriteLine(Enum.IsDefined(typeof(CarType), "C1")); //true
+            //Console.WriteLine(Enum.IsDefined(typeof(CarType), "0")); //false
+            //Console.WriteLine(Enum.IsDefined(typeof(CarType), 0)); //true
+            //Console.WriteLine(Enum.IsDefined(typeof(CarType), 10));//false
+
+            //var car1 = Parse<CarType>("2");
+
+            //ProctolType pType = (ProctolType)0;
+
+            //Console.WriteLine(ProctolType.N1 == ProctolType.N2); //相等
+            //Console.WriteLine(d is Enum);
+
+
+            ////获取枚举所属类型
+            //Type foo4 = Enum.GetUnderlyingType(typeof(CarType));
+
+            ////获取所有枚举成员
+            //Array foo5 = Enum.GetValues(typeof(CarType));
+
+            ////获取所有枚举成员的字段名
+            //string[] foo6 = Enum.GetNames(typeof(CarType));
+
+            //var user = default(AdminType);
+            //Console.WriteLine(user); //0
+            //Console.WriteLine(Enum.IsDefined(typeof(AdminType), 0)); //false
+
+            Stopwatch watch = new Stopwatch();
+
+            Enum userEnum = UserType.typeY;
+
+            watch.Start();
+
+            for (int i = 0; i < 10000000; i++)
             {
-                Console.WriteLine(value + "--" + (int)value);//获取名称和值
+                var t = Convert.ToInt32(userEnum);
             }
+            watch.Stop();
 
-            //Type? ssss = null;
+            Console.WriteLine(watch.ElapsedMilliseconds);
 
-            //int i = (int)ssss;
+            watch.Restart();
+            var intTYpe = typeof(int);
+            for (int i = 0; i < 10000000; i++)
+            {
+                if (userEnum.GetType().UnderlyingSystemType == intTYpe)
+                {
+                    //var t = userEnum.GetHashCode();
+                }
+                var t = userEnum.GetHashCode();
+            }
+            watch.Stop();
 
-            CarType result;
-            Console.WriteLine("CarType100");
-            Console.WriteLine(Enum.Parse<CarType>("100")); // 100
-            Console.WriteLine(Enum.TryParse("C1", out result)); //true
-            Console.WriteLine(Enum.TryParse("C5", out result)); //false
-            Console.WriteLine(Enum.TryParse("0", out result)); //true
-            Console.WriteLine(Enum.TryParse("10", out result)); //true
-
-            Console.WriteLine("-10" + Enum.TryParse("-10", out result)); //true
-
-            Console.WriteLine(Enum.IsDefined(typeof(CarType), "C1")); //true
-            Console.WriteLine(Enum.IsDefined(typeof(CarType), "0")); //false
-            Console.WriteLine(Enum.IsDefined(typeof(CarType), 0)); //true
-            Console.WriteLine(Enum.IsDefined(typeof(CarType), 10));//false
-
-            var car1 = Parse<CarType>("2");
-
-            ProctolType pType = (ProctolType)0;
-
-            Console.WriteLine(ProctolType.N1 == ProctolType.N2); //相等
-            Console.WriteLine(d is Enum);
-
-
-            //获取枚举所属类型
-            Type foo4 = Enum.GetUnderlyingType(typeof(CarType));
-
-            //获取所有枚举成员
-            Array foo5 = Enum.GetValues(typeof(CarType));
-
-            //获取所有枚举成员的字段名
-            string[] foo6 = Enum.GetNames(typeof(CarType));
-
-            var user = default(AdminType);
-            Console.WriteLine(user); //0
-            Console.WriteLine(Enum.IsDefined(typeof(AdminType), 0)); //false
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            Contract.Assert(true, "");
 
             Console.ReadKey();
         }
@@ -120,7 +151,7 @@ namespace EnumTest
         public UserType type { get; set; }
     }
 
-    public enum UserType
+    public enum UserType:ushort
     {
         typeX,
         typeY
