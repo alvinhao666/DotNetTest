c36ceba48a47a076e31607e560a5fe126284f7d0
rongguohao <843468011@qq.com>
2022-02-22 13:16:51 +0800

    init

---
+++ 对象映射/MapsterTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using Mapster;
+using System;
+
+
+namespace MapsterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User { Name = "张三", Age = 10 };
+
+            var input =new User { Name = "张三3",Age = 11 };
+
+
+            var user2 = input.Adapt(user);
+
+
+            Console.WriteLine(user.Name + "  " +user.Age);
+
+            Console.WriteLine(user2.Name + "  " + user2.Age);
+
+            Console.WriteLine(user2 == user);
+
+            var userNew = user.Adapt<User>();
+
+            Console.WriteLine(userNew == user);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public class User
+    {
+        public string Name { get; set; }
+
+
+        public string Password { get; set; }
+
+
+        public int Age { get; set; }
+    }
+}
