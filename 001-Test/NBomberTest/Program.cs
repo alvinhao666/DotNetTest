9261ba9df600c9af9d08263d193419a8a8e7714e
rongguohao <843468011@qq.com>
2023-01-09 10:27:00 +0800

    init

---
+++ 001-Test/NBomberTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using FSharp.Json;
+using NBomber;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+namespace NBomberTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var httpClient = new HttpClient();
+
+            var step = Step.Create("fetch_login", async context =>
+            {
+                var jsonData = new { CaptchaCode = "vyck", CaptchaId = "91b222edfba64fe1b3d7683c7bc3afa4" };
+
+                var requestContent = new StringContent(JsonSerializer.Serialize(jsonData), Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PostAsync("http://localhost:8000/Dev/auth/login", requestContent);
+
+                return response.IsSuccessStatusCode
+                    ? Response.Ok()
+                    : Response.Fail();
+            });
+
+            var scenario = ScenarioBuilder.CreateScenario("loginapi", step);
+
+            NBomberRunner
+                .RegisterScenarios(scenario)
+                .Run();
+
+        }
+    }
+}
