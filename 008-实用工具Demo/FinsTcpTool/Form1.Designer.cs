117a17d9ef1920fbbb7461a3657ebd253eafd881
rongguohao <843468011@qq.com>
2023-06-20 17:31:34 +0800

    init

---
+++ 008-实用工具Demo/FinsTcpTool/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace FinsTcpTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_IP = new System.Windows.Forms.Label();
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_IP
+            // 
+            this.lbl_IP.AutoSize = true;
+            this.lbl_IP.Location = new System.Drawing.Point(12, 26);
+            this.lbl_IP.Name = "lbl_IP";
+            this.lbl_IP.Size = new System.Drawing.Size(41, 12);
+            this.lbl_IP.TabIndex = 0;
+            this.lbl_IP.Text = "IP地址";
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(59, 17);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(139, 21);
+            this.txt_IP.TabIndex = 1;
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(59, 52);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(139, 21);
+            this.txt_Port.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "端口号";
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(221, 17);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(75, 56);
+            this.btn_Connect.TabIndex = 4;
+            this.btn_Connect.Text = "连接";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(315, 17);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 56);
+            this.btn_Close.TabIndex = 5;
+            this.btn_Close.Text = "断开";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 172);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.txt_Port);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_IP);
+            this.Controls.Add(this.lbl_IP);
+            this.Name = "Form1";
+            this.Text = "FinsTcp测试工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_IP;
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.TextBox txt_Port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Button btn_Close;
+    }
+}
+
---
+++ 008-实用工具Demo/FinsTcpTool/Form1.cs
@@ -0,0 +1,81 @@
+﻿using OmronFinsTCP.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinsTcpTool
+{
+    public partial class Form1 : Form
+    {
+        EtherNetPLC _etherNetPLC = new EtherNetPLC();
+
+        private string _ip;
+
+        private ushort _port;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 连接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            _ip = txt_IP.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(_ip))
+            {
+                MessageBox.Show("请填写IP地址");
+                return;
+            }
+
+            if (!Regex.IsMatch(_ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$"))
+            {
+                MessageBox.Show("IP数据异常，请检查");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txt_IP.Text))
+            {
+                MessageBox.Show("请填写端口号");
+                return;
+            }
+
+            if (ushort.TryParse(txt_Port.Text.Trim(), out ushort port))
+            {
+                _port = port;
+            }
+            else
+            {
+                MessageBox.Show("端口号异常，请检查");
+                return;
+            }
+
+            if (_etherNetPLC.Link(_ip, (short)_port, 1000) < 0)
+            {
+                MessageBox.Show("连接失败",);
+            }
+        }
+
+        /// <summary>
+        /// 断开
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
