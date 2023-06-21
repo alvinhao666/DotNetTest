0204cb34429a2e928e6f76989a5cea9e30ccb8e8
rongguohao <843468011@qq.com>
2023-06-21 09:21:13 +0800

    init

--- 008-实用工具Demo/FinsTcpTool/Form1.Designer.cs
+++ 008-实用工具Demo/FinsTcpTool/Form1.Designer.cs
@@ -34,50 +34,51 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btn_Connect = new System.Windows.Forms.Button();
             this.btn_Close = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_IP
             // 
             this.lbl_IP.AutoSize = true;
-            this.lbl_IP.Location = new System.Drawing.Point(24, 52);
-            this.lbl_IP.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_IP.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_IP.Location = new System.Drawing.Point(12, 26);
             this.lbl_IP.Name = "lbl_IP";
-            this.lbl_IP.Size = new System.Drawing.Size(82, 24);
+            this.lbl_IP.Size = new System.Drawing.Size(50, 20);
             this.lbl_IP.TabIndex = 0;
             this.lbl_IP.Text = "IP地址";
             // 
             // txt_IP
             // 
-            this.txt_IP.Location = new System.Drawing.Point(118, 34);
-            this.txt_IP.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txt_IP.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_IP.Location = new System.Drawing.Point(69, 23);
             this.txt_IP.Name = "txt_IP";
-            this.txt_IP.Size = new System.Drawing.Size(274, 35);
+            this.txt_IP.Size = new System.Drawing.Size(139, 23);
             this.txt_IP.TabIndex = 1;
             // 
             // txt_Port
             // 
-            this.txt_Port.Location = new System.Drawing.Point(118, 104);
-            this.txt_Port.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txt_Port.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_Port.Location = new System.Drawing.Point(69, 58);
             this.txt_Port.Name = "txt_Port";
-            this.txt_Port.Size = new System.Drawing.Size(274, 35);
+            this.txt_Port.Size = new System.Drawing.Size(139, 23);
             this.txt_Port.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 122);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "端口号";
             // 
             // btn_Connect
             // 
-            this.btn_Connect.Location = new System.Drawing.Point(442, 34);
-            this.btn_Connect.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btn_Connect.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Connect.Location = new System.Drawing.Point(224, 23);
             this.btn_Connect.Name = "btn_Connect";
-            this.btn_Connect.Size = new System.Drawing.Size(150, 112);
+            this.btn_Connect.Size = new System.Drawing.Size(75, 56);
             this.btn_Connect.TabIndex = 4;
             this.btn_Connect.Text = "连接";
             this.btn_Connect.UseVisualStyleBackColor = true;
@@ -85,29 +86,41 @@
             // 
             // btn_Close
             // 
-            this.btn_Close.Location = new System.Drawing.Point(630, 34);
-            this.btn_Close.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btn_Close.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Close.Location = new System.Drawing.Point(315, 23);
             this.btn_Close.Name = "btn_Close";
-            this.btn_Close.Size = new System.Drawing.Size(150, 112);
+            this.btn_Close.Size = new System.Drawing.Size(75, 56);
             this.btn_Close.TabIndex = 5;
             this.btn_Close.Text = "断开";
             this.btn_Close.UseVisualStyleBackColor = true;
             this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
             // 
+            // btn_Save
+            // 
+            this.btn_Save.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Save.Location = new System.Drawing.Point(407, 23);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 56);
+            this.btn_Save.TabIndex = 6;
+            this.btn_Save.Text = "保存参数";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(848, 344);
+            this.ClientSize = new System.Drawing.Size(507, 172);
+            this.Controls.Add(this.btn_Save);
             this.Controls.Add(this.btn_Close);
             this.Controls.Add(this.btn_Connect);
             this.Controls.Add(this.txt_Port);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_IP);
             this.Controls.Add(this.lbl_IP);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "FinsTcp测试工具";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -122,6 +135,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_Connect;
         private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Save;
     }
 }
 
--- 008-实用工具Demo/FinsTcpTool/Form1.cs
+++ 008-实用工具Demo/FinsTcpTool/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace FinsTcpTool
 {
@@ -27,9 +28,18 @@ namespace FinsTcpTool
             InitializeComponent();
         }
 
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            txt_IP.SelectionStart = 0;
+            txt_IP.SelectionLength = 0;
+            btn_Close.Enabled = false;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.btn_Close.Enabled = false;
+            LocalConfig.Instance.LoadConfig();
+            txt_IP.Text = LocalConfig.Instance.IP;
+            txt_Port.Text = LocalConfig.Instance.Port.ToString();
         }
 
         /// <summary>
@@ -39,35 +49,7 @@ namespace FinsTcpTool
         /// <param name="e"></param>
         private async void btn_Connect_Click(object sender, EventArgs e)
         {
-            _ip = txt_IP.Text.Trim();
-
-            if (string.IsNullOrWhiteSpace(_ip))
-            {
-                MessageBox.Show("请填写IP地址", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
-            if (!Regex.IsMatch(_ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$"))
-            {
-                MessageBox.Show("IP数据异常，请检查", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            if (string.IsNullOrWhiteSpace(txt_IP.Text))
-            {
-                MessageBox.Show("请填写端口号", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
-            if (ushort.TryParse(txt_Port.Text.Trim(), out ushort port))
-            {
-                _port = port;
-            }
-            else
-            {
-                MessageBox.Show("端口号异常，请检查", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
+            if (!ValidateParam()) return;
 
             try
             {
@@ -139,6 +121,64 @@ namespace FinsTcpTool
             {
                 MessageBox.Show($"断开失败! {ex}", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        /// <summary>
+        /// 保存参数配置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            if (!ValidateParam()) return;
+
+            if (LocalConfig.Instance.IP == _ip && LocalConfig.Instance.Port == _port) return;
+
+            LocalConfig.Instance.IP = _ip;
+
+            LocalConfig.Instance.Port = _port;
+
+            LocalConfig.Instance.SaveConfig();
+        }
+
+
+        /// <summary>
+        /// 验证
+        /// </summary>
+        /// <returns></returns>
+        private bool ValidateParam()
+        {
+            _ip = txt_IP.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(_ip))
+            {
+                MessageBox.Show("请填写IP地址", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (!Regex.IsMatch(_ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$"))
+            {
+                MessageBox.Show("IP数据异常，请检查", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txt_Port.Text))
+            {
+                MessageBox.Show("请填写端口号", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (ushort.TryParse(txt_Port.Text.Trim(), out ushort port))
+            {
+                _port = port;
+            }
+            else
+            {
+                MessageBox.Show("端口号异常，请检查", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
         }
     }
 }
