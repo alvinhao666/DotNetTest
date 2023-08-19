ec218475e76bb64848b02ba704b7f9bce6fffb30
rongguohao <843468011@qq.com>
2023-08-19 22:34:45 +0800

    init

---
+++ 001-Test/DeleteFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileOperateProxy.Delete(@"C:\Users\rongguohao\Desktop\a.txt");
+
+            Console.ReadKey();
+        }
+    }
+}
