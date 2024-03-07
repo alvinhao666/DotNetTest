bbb596ae0ff8fb413db6ea26db8ebbb585bed436
rongguohao <843468011@qq.com>
2024-03-07 11:15:36 +0800

    init

---
+++ 001-Test/AsyncLocalTest/Program.cs
@@ -0,0 +1,64 @@
+﻿//AsyncLocal<string> test = new AsyncLocal<string>();
+
+//test.Value = "111";
+
+//await Test1();
+
+//Console.WriteLine(test.Value);
+
+//Test2();
+
+//Console.WriteLine(test.Value);
+
+//Console.ReadKey();
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTest
+{
+    class Program
+    {
+       static AsyncLocal<string> test = new AsyncLocal<string>();
+
+        static async Task Main(string[] args)
+        {
+            test.Value = "111";
+
+            Test1();
+
+            Console.WriteLine(test.Value);
+
+            await Test1();
+
+            Console.WriteLine(test.Value);
+
+            Test2();
+
+            Console.WriteLine(test.Value);
+
+            Console.ReadKey();
+        }
+
+
+        static Task Test1()
+        {
+            test.Value = "222";
+
+            return Task.CompletedTask;
+        }
+
+
+        static async Task Test2()
+        {
+            test.Value = "333";
+        }
+
+    }
+}
+
+
