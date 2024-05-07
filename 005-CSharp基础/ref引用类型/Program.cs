f43bb0951a872b156949794bc7936c3d06d44673
rongguohao <843468011@qq.com>
2024-05-08 00:21:26 +0800

    init

--- 005-CSharp基础/ref引用类型/Program.cs
+++ 005-CSharp基础/ref引用类型/Program.cs
@@ -8,15 +8,12 @@ namespace ref引用类型
         {
 
             List<string> list = new List<string> { "a", "ab", "aaa", "bc" };
-            Console.WriteLine(getMemory(list));
             TestList(ref list);
             Console.WriteLine("Main方法里的list现在有{0}个元素", list.Count());
         }
         public static void TestList(ref List<string> list)
         {
-            Console.WriteLine(getMemory(list));
             list = list.Where(p => p.Contains("a")).ToList();
-            Console.WriteLine(getMemory(list));
             Console.WriteLine("TestList方法里的list现在有{0}个元素", list.Count());
         }
 
@@ -34,14 +31,5 @@ namespace ref引用类型
         //    Console.WriteLine(getMemory(list));
         //    Console.WriteLine("TestList方法里的list现在有{0}个元素", list.Count());
         //}
-
-        //获取引用类型的内存地址方法
-        public static string getMemory(object obj)
-        {
-            GCHandle handle = GCHandle.Alloc(obj, GCHandleType.WeakTrackResurrection);
-            IntPtr addr = GCHandle.ToIntPtr(handle);
-            return $"0x{addr.ToString("X")}";
-        }
-
     }
 }
